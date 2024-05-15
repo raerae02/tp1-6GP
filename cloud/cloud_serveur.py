158e1b2712ed736ec58976f5506df6433b81942e
Eliefrh <2067396@bdeb.qc.ca>
2024-05-15 03:12:28 -0400

    petite modification de test

--- cloud/cloud_serveur.py
+++ cloud/cloud_serveur.py
@@ -130,7 +130,10 @@ def envoyer_commande_objet(id_objet, command):
             response = requests.post(pi_url, json={"command": command}, timeout=10)
             response.raise_for_status()
             print(f"Command '{command}' sent to Raspberry Pi {id_objet}: {response.json()}")
+            if command == "localise":
+                 clignoter_led(id_objet)
             return response.json()
+        
     except requests.exceptions.RequestException as e:
         print(f"Failed to send command '{command}' to Raspberry Pi {id_objet}: {e}")
         return {"success": False}
@@ -245,6 +248,7 @@ def save_video_in_database(nom_video, id_objet, video_size):
 
 @app.route('/clignoter_led/<int:id_objet>', methods=['POST'])
 def clignoter_led(id_objet):
+    
     if ControleurVideos:
         print(f"Clignotement de la LED du Raspberry Pi {id_objet}hahahahahaha" )
         print(f"Clignotement de la LED du Raspberry Pi {id_objet}hahahahahaha" )
