cfcc659c85e4d7cf17183935147a9c3ebe0e7d56
Eliefrh <2067396@bdeb.qc.ca>
2024-05-15 02:39:39 -0400

    add print

--- cloud/cloud_serveur.py
+++ cloud/cloud_serveur.py
@@ -246,6 +246,10 @@ def save_video_in_database(nom_video, id_objet, video_size):
 @app.route('/clignoter_led/<int:id_objet>', methods=['POST'])
 def clignoter_led(id_objet):
     if ControleurVideos:
+        print(f"Clignotement de la LED du Raspberry Pi {id_objet}hahahahahaha" )
+        print(f"Clignotement de la LED du Raspberry Pi {id_objet}hahahahahaha" )
+        print(f"Clignotement de la LED du Raspberry Pi {id_objet}hahahahahaha" )
+        
         ControleurVideos.clignoter_led(id_objet, 3)  # Clignoter la LED 3 fois
         return jsonify({"success": True, "message": "LED clignotée"})
     else:
